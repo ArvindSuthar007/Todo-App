5860dbd6cb65cbcbbc662bafa97443f93875a015
Arvind_Suthar <arvindsutharaasday@gmail.com>
2024-03-14 13:20:14 +0530

    fonts styles added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,14 @@
 export default function NavBar() {
   return (
     <>
-      <h1>To-Do List</h1>
+      <h1
+        style={{
+          fontFamily: "MainFont, sans-serif",
+          fontWeight: "100",
+        }}
+      >
+        ToDo App
+      </h1>
     </>
   );
 }
